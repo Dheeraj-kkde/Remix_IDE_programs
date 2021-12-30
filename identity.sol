6a8d25fa40da7e9bc84a22805207ea1f18b34dda
Dheeraj Kumar <91405138+Dheeraj-kkde@users.noreply.github.com>
2021-12-30 08:08:14 +0530

    basic program

---
+++ identity.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Identity
+{
+    string name;
+    uint age;
+
+    constructor() public
+    {
+        name="Dheeraj";
+        age=26;
+    }
+
+    function getName() view public returns(string memory)
+    {
+        return name;
+    }
+
+    function getAge() view public returns(uint)
+    {
+        return age;
+    }
+
+    function setAge() public
+    {
+        age = age+1;
+    }
+
+}
