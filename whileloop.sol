92790c8400019327b7cf0f7ebf1f5739ffed4401
Dheeraj Kumar <91405138+Dheeraj-kkde@users.noreply.github.com>
2022-01-10 12:26:33 +0530

    Create whileloop.sol
    basic program demonstrating how to use while loop in solidity launguage

---
+++ whileloop.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >0.5.0 <=0.9.0;
+
+contract Whileloop
+{
+    uint[3] public arr;
+    uint public count;
+
+    function loop()public
+    {
+
+        while(count<arr.length)
+        {
+            arr[count]=count;
+            count=count+1;
+        }
+        
+    }
+}
